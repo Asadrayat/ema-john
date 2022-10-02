5eca7014732e005dfac3338b9c4a3e98485fbc4c
Asadrayat <asad.rayat4@gmail.com>
2022-10-02 13:25:01 +0600

    local storage added

---
+++ src/Component/Cart/Cart.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import'./Cart.css'
+const Cart = (props) => {
+    const {cart} = props;
+    let total =0;
+    let shipping = 0;
+    for (const product of cart){
+        total = product.price + total;
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = shipping + total + tax;
+    // console.log(cart);
+    return (
+        <div className='cart'>
+            <h1>Summery</h1>
+            <h3>Selected Item : {cart.length}</h3>
+            <h4>Total Price : ${total} </h4>
+            <h4>Total Shipping Charges : ${shipping}</h4>
+            <h4>Tax : ${tax} </h4>
+            <h3>Grand Total : {grandTotal}</h3>
+
+        </div>
+    );
+};
+
+export default Cart;
